ed4abb8a8846c1d7b5fc555200c1b6d72e7541f2
nico.gouvernet <nicolas-gouvernet@outlook.fr>
2017-12-18 11:10:39 +0100

    Mise en place du css

--- index.php
+++ index.php
@@ -1,9 +1,21 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>TF1</title>
+	<style>
+        body {
+            background-color: #B78178;
+            color: white;  
+            text-align: center; 
+            font-family:sans-serif;
+            font-size: 1.2em;
+            }
+             img {
+            height: 200px;
+            border-radius: 10px;
+            }
+    </style>
 </head>
-<body>
 <body>
 <p> Veuillez entrer le mot de passe pour accéder au fichier <strong>secret.php</strong></p>
 <form action="password.php" method="post">
@@ -14,6 +26,5 @@
         </form>
         <p> Si vous ne travaillez pas chez TF1 ;) , inutile d'insister vous ne trouverez jamais le mot de passe ! pour accéder au archives du JT;-)</p>
          <img src="images/logo.png" alt="Photo de TF1" title="TF1"/>
-</body>
 </body>
 </html>
