7450dd9b6e84ee5cef037e22486e1eb1a229e239
nico.gouvernet <nicolas-gouvernet@outlook.fr>
2017-12-18 11:16:30 +0100

    Mise en place du mot de passe kangourou

--- password.php
+++ password.php
@@ -4,6 +4,10 @@
 	<title></title>
 </head>
 <body>
-
+ <?php
+    if (isset($_POST['password']) AND $_POST['password'] ==  "kangourou") // Mot de passe correct
+    {
+// nous mettons en places le code suivant :
+?>
 </body>
 </html>
