fa4260a31e26be445e3cdfa9ad8b2cfbab3a30b6
nico.gouvernet <nicolas-gouvernet@outlook.fr>
2017-12-18 11:08:03 +0100

    Reprise du body du TT1 Mot de passe

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<body>
+<p> Veuillez entrer le mot de passe pour accéder au fichier <strong>secret.php</strong></p>
+<form action="password.php" method="post">
+            <p>
+            <input type="password" name="password" />
+            <input type="submit" value="Valider" />
+            </p>
+        </form>
+        <p> Si vous ne travaillez pas chez TF1 ;) , inutile d'insister vous ne trouverez jamais le mot de passe ! pour accéder au archives du JT;-)</p>
+         <img src="images/logo.png" alt="Photo de TF1" title="TF1"/>
+</body>
+</body>
+</html>
