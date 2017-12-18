bd2f6dc5ee88e96601bfe6e0e4de84c8a75c3781
nico.gouvernet <nicolas-gouvernet@outlook.fr>
2017-12-18 11:28:16 +0100

    Formulaire Pseudo et message

--- secret.php
+++ secret.php
@@ -4,6 +4,13 @@
 	<title></title>
 </head>
 <body>
+<form action="minichat_post.php" method="post">
+        <p>
+        <label for="pseudo">Pseudo</label> : <input type="text" name="pseudo" id="pseudo" /><br />
+        <label for="message">Message</label> :  <input type="text" name="message" id="message" /><br />
 
+        <input type="submit" value="Envoyer" />
+    </p>
+    </form>
 </body>
 </html>
