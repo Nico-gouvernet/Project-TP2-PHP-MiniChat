56332d83cc9b1fb229acae3ddce63c190c5d55cc
nico.gouvernet <nicolas-gouvernet@outlook.fr>
2017-12-18 11:17:21 +0100

    Mise en place de contenu

--- password.php
+++ password.php
@@ -9,5 +9,12 @@
     {
 // nous mettons en places le code suivant :
 ?>
+<h1>Voilà le code :</h1>
+        <p><strong>KK09-365I-C585-JDFM-DSFN-78D1-FS28-SFFV</strong></p>   
+        
+        <p>
+        Cette page est pour le personnel de TF1. Attention N'oubliez pas que les codes d'accès sont changés régulièrement.<br />
+        TF1 vous remercie de votre visite.
+        </p>
 </body>
 </html>
